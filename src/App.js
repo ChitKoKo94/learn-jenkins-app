892886a56fb8e42928909a48df6f03a678a74002
Chit Ko <74890903+ChitKoKo94@users.noreply.github.com>
2025-03-25 00:44:41 +0000

    Update html

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div class="container">
+          <h1>CI-CD Project Overview</h1>
+          <p>This application is running on AWS ECS service and Jenkins pipeline automates the end-to-end process of building, containerizing, and deploying of the project.</p>
+          <h2>Workflow Steps:</h2>
+          <ul>
+              <li>Checks out the latest code from the <strong>GitHub repository</strong>.</li>
+              <li>Builds the project and creates a <strong>Docker image</strong>.</li>
+              <li>Utilize <strong>AWS CLI Docker image</strong> to push the Docker image to <strong>Amazon Elastic Container Registry (ECR)</strong>.</li>
+              <li>Deploys the updated image to an <strong>AWS ECS service</strong>.</li>
+          </ul>
+          <div class="footer">
+              <p>&copy; 2025 Jenkins Automation Project</p>
+          </div>
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://example.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Jenkins on Udemy
-        </a>
       </header>
       <p>
           Application version: {process.env.REACT_APP_VERSION}
