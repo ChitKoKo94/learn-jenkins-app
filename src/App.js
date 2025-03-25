793532a2f8ae50e593078d50797d29248ab9d7da
Chit Ko <74890903+ChitKoKo94@users.noreply.github.com>
2025-03-25 01:01:20 +0000

    Styling update

--- src/App.js
+++ src/App.js
@@ -5,25 +5,25 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <div className="container">
           <h1>CI-CD Project Overview</h1>
-          <p>This application is running on AWS ECS service and Jenkins pipeline automates the end-to-end process of building, containerizing, and deploying of the project.</p>
-          <h2>Workflow Steps:</h2>
-          <ul>
-              <li>Checks out the latest code from the <strong>GitHub repository</strong>.</li>
-              <li>Builds the project and creates a <strong>Docker image</strong>.</li>
-              <li>Utilize <strong>AWS CLI Docker image</strong> to push the Docker image to <strong>Amazon Elastic Container Registry (ECR)</strong>.</li>
-              <li>Deploys the updated image to an <strong>AWS ECS service</strong>.</li>
-          </ul>
-          <div className="footer">
-              <p>&copy; 2025 Jenkins Automation Project</p>
+          <div className='projectContent'>
+            <p>This application is running on AWS ECS service and Jenkins pipeline automates the end-to-end process of building, containerizing, and deploying of the project.</p>
+            <h2>Workflow Steps:</h2>
+            <ul>
+                <li>Checks out the latest code from the <strong>GitHub repository</strong>.</li>
+                <li>Builds the project and creates a <strong>Docker image</strong>.</li>
+                <li>Utilize <strong>AWS CLI Docker image</strong> to push the Docker image to <strong>Amazon Elastic Container Registry (ECR)</strong>.</li>
+                <li>Deploys the updated image to an <strong>AWS ECS service</strong>.</li>
+            </ul>
+            <div className="footer">
+                <p>&copy; 2025 Jenkins Automation Project | Application version: {process.env.REACT_APP_VERSION}</p>
+            </div>
           </div>
         </div>
-        <img src={logo} className="App-logo" alt="logo" />
+        
       </header>
-      <p>
-          Application version: {process.env.REACT_APP_VERSION}
-      </p>
     </div>
   );
 }
